ed190c124851101181ae00d242df68857489bfb2
King-of-Roses <alfonskrookprivat@gmail.com>
2024-05-16 11:18:43 +0200

    Update StringBox.java

--- iMatApp/src/imat/StringBox.java
+++ iMatApp/src/imat/StringBox.java
@@ -4,14 +4,17 @@ public class StringBox {
     public String strBox ="";
     public int min;
     public int max;
-    public boolean number;
-    public boolean special;
-    public StringBox(int mIn, int mAx, boolean nUmber, boolean sPecial)
+    public boolean onlyAcceptNumber;
+    public boolean onlyAcceptNoneSpecialChar;
+
+    public String OnlyAcceptNoneSpecialCharList;
+    public StringBox(int min, int max, boolean onlyAcceptNumber, boolean onlyAcceptNoneSpecialChar)
     {
-        min = mIn;
-        max = mAx;
-        number = nUmber;
-        special = sPecial;
+        this.min = min;
+        this.max = max;
+        this.onlyAcceptNumber = onlyAcceptNumber;
+        this.onlyAcceptNoneSpecialChar = onlyAcceptNoneSpecialChar;
+        this.OnlyAcceptNoneSpecialCharList="qwertyuiopåasdfghjklöäzxcvbnm1234567890@'-";
     }
 
     void errorBox(String Message){
@@ -28,14 +31,27 @@ public class StringBox {
     }
 
     void confirmBox(){
-        if (number)
+        if (onlyAcceptNumber)
             for(int i = 0; i < strBox.length(); i++)
                 if (strBox.charAt(i) < '0' || strBox.charAt(i) > '9') {
                     errorBox("Vänligen skriv bara siffror 0-9");
                     break;
                 }
-        if ((strBox.length() < min) || (strBox.length() > max)){
-
+        else-if(onlyAcceptNoneSpecialChar){
+            for(int i = 0; i < strBox.length(); i++){
+                if (OnlyAcceptNoneSpecialCharList.indexOf(strBox.toLowerCase().charAt(i))==-1){
+                    errorBox("Vänligen skriv ");
+                    break;}
+            }
         }
+
+        if ((strBox.length() < min) || (strBox.length() > max)){
+            if (min == max){
+                errorBox("Vänligen skriv "+max+" siffror");}
+            else {
+                errorBox("Skriv "+min+"-"+max+" karaktärer");
+            }
+        }
+
     }
 }
