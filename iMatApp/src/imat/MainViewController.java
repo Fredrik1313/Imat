f1815836a752c34d2c9d643ac855a913b8647222
William Thompson <89342519+ThompaCode@users.noreply.github.com>
2024-05-17 14:37:31 +0200

    Revert "Start på flera saker"

--- iMatApp/src/imat/MainViewController.java
+++ iMatApp/src/imat/MainViewController.java
@@ -9,47 +9,26 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
-import se.chalmers.cse.dat216.project.ProductCategory;
 import se.chalmers.cse.dat216.project.ShoppingItem;
-import se.chalmers.cse.dat216.project.ShoppingCart;
 
 public class MainViewController implements Initializable {
 
-    @FXML
-    Label pathLabel;
+
 
     IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
-    iMatBackendController backendController =  new iMatBackendController();
+    private iMatBackendController backendController =  new iMatBackendController();
     public void initialize(URL url, ResourceBundle rb) {
+
         String iMatDirectory = iMatDataHandler.imatDirectory();
-        pathLabel.setText(iMatDirectory);
-
     }
 
-    //Efter genomfört köp så kör man denna funktionen med kundvagnen som en lista av ShoppingItems för att uppdatera listan med frekvent köpta objekt.
-    void UpdateFrequentlyPurchasedList(List<ShoppingItem> shoppingItemsList) {
+        //Efter köp genomfört så kör man denna funktionen med kundvagnen som en lista av ShoppingItems för att uppdatera listan med frekvent köpta objekt.
+    public void UpdateFrequentlyPurchasedList(List<ShoppingItem> shoppingItemsList) {
         backendController.addToMostPurchasedProducts(shoppingItemsList);
-    }
 
-    void UpdateCurrentProducts(List<Product> productsList)
-    {
 
     }
 
-    void AddToCart(Product product,Double amountToAdd)
-    {
-        iMatDataHandler.getShoppingCart().addProduct(product,amountToAdd);
-    }
-    void AddToCart(Product product)
-    {
-        iMatDataHandler.getShoppingCart().addProduct(product);
-    }
-
-
-    public void openDetailedProductView(Product product) {
-
-    }
 }
--- iMatApp/src/imat/ProductItemController.java
+++
@@ -1,5 +0,0 @@
-package imat;
-
-public class ProductItemController {
-
-}
