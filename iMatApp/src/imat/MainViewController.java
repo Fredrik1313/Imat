ffc98f023fee32f6d8368e59c43985f23ef45375
William Thompson <wpet01@live.se>
2024-05-17 14:20:39 +0200

    Start på flera saker
    Start på produktItems, liten kundvags items och stor kundvagsnitem Samt kategorier

--- iMatApp/src/imat/MainViewController.java
+++ iMatApp/src/imat/MainViewController.java
@@ -9,9 +9,12 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
+import se.chalmers.cse.dat216.project.ProductCategory;
 import se.chalmers.cse.dat216.project.ShoppingItem;
+import se.chalmers.cse.dat216.project.ShoppingCart;
 
 public class MainViewController implements Initializable {
 
@@ -19,21 +22,34 @@ public class MainViewController implements Initializable {
     Label pathLabel;
 
     IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
-    private iMatBackendController backendController =  new iMatBackendController();
+    iMatBackendController backendController =  new iMatBackendController();
     public void initialize(URL url, ResourceBundle rb) {
-
         String iMatDirectory = iMatDataHandler.imatDirectory();
         pathLabel.setText(iMatDirectory);
 
-
-
     }
 
-        //Efter köp genomfört så kör man denna funktionen med kundvagnen som en lista av ShoppingItems för att uppdatera listan med frekvent köpta objekt.
-    public void UpdateFrequentlyPurchasedList(List<ShoppingItem> shoppingItemsList) {
+    //Efter genomfört köp så kör man denna funktionen med kundvagnen som en lista av ShoppingItems för att uppdatera listan med frekvent köpta objekt.
+    void UpdateFrequentlyPurchasedList(List<ShoppingItem> shoppingItemsList) {
         backendController.addToMostPurchasedProducts(shoppingItemsList);
+    }
 
+    void UpdateCurrentProducts(List<Product> productsList)
+    {
 
     }
 
+    void AddToCart(Product product,Double amountToAdd)
+    {
+        iMatDataHandler.getShoppingCart().addProduct(product,amountToAdd);
+    }
+    void AddToCart(Product product)
+    {
+        iMatDataHandler.getShoppingCart().addProduct(product);
+    }
+
+
+    public void openDetailedProductView(Product product) {
+
+    }
 }
